475c259366cbcde213c25ca209a2d00bdb051d28
Egor Kurkin <proehavshiynapravo@yandex.ru>
2021-07-28 02:09:08 +0300

    feat: static output is configured

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 // включает основную логику сервера, запуск и подключение к базе данных;
 
 const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const fs = require('fs').promises;
 
 const { PORT = 3000 } = process.env;
-const app = express();
 const bodyParser = require('body-parser');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 const { setCors } = require('./middlewares/middlewares');
 
+const app = express();
+
+// подключаемся к серверу mongo
+// mongoose.connect();
+
+// отдача статики
+app.use(express.static(path.resolve(__dirname, 'public')));
 // мидлвара управления CORS
 app.use(setCors);
 // мидлвара для собирания тела request JSON-формата
