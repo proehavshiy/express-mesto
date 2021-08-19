5f6e7b936ffee0969317867a65fe06c484e053ac
Egor Kurkin <proehavshiynapravo@yandex.ru>
2021-08-19 15:50:05 +0300

    fix: cors has been modified

--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -11,18 +11,32 @@ function setNoCacheHeaders(req, res, next) {
 // Массив разешённых доменов
 const allowedCors = [
   'localhost:3000',
-  'https://www.google.ru',
+  'front.mesto15.nomoredomains.club',
 ];
 
+// eslint-disable-next-line consistent-return
 function setCors(req, res, next) {
   // Записываем в переменную origin соответствующий заголовок
   const { origin } = req.headers;
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+  const requestHeaders = req.headers['access-control-request-headers']; // сохраняем список заголовков исходного запроса
+
   // Проверяем, что значение origin есть среди разрешённых доменов
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+
+  // Если это предварительный запрос, добавляем нужные заголовки
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    // разрешаем кросс-доменные запросы с этими заголовками
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    // завершаем обработку запроса и возвращаем результат клиенту
+    return res.end();
+  }
+  // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  // res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   next();
 }
 
